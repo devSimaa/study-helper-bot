b3b7c5cdb546892d1defcbbfc39e53d55d6860f6
Sima3443 <toppochka@gmail.com>
2023-09-04 23:30:59 +0300

    delete comment

--- app/handlers/user/schedule.py
+++ app/handlers/user/schedule.py
@@ -3,10 +3,7 @@ from loader import dp, bot
 from app.keyboards.inline_keyboard import schedule_ikb
 from app.others.document_reader import doc_reader
 
-# ...document.txt_reader.reader_txt(r"D:\010101010\python\Bots\study-bot\document\schedule_1.txt")
 
-
-# распиание
 @dp.message_handler(commands="schedule")
 async def schedule_command(message: types.Message):
     await message.answer(text=doc_reader("schedule_1"), reply_markup=schedule_ikb(1))
